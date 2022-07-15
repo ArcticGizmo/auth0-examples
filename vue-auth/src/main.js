91896a1d90dedd47c5d0a135ef4b9a9c567e969d
Jonathan Howell <jon.howell314@gmail.com>
2022-07-15 10:28:01 +0800

    🐛 fixed incorrect prop for auth

--- vue-auth/src/main.js
+++ vue-auth/src/main.js
@@ -9,7 +9,7 @@ import Icon from './components/Icon.vue';
 const auth = createAuth0({
   domain: import.meta.env['VITE_AUTH0_DOMAIN'],
   client_id: import.meta.env['VITE_AUTH0_CLIENT_ID'],
-  redirect_URI: window.location.origin,
+  redirect_uri: window.location.origin,
 });
 
 const app = createApp(App);
