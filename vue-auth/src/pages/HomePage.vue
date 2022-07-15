00ea7435f727f189d329bbc8c8a4d33f99c8596d
Jonathan Howell <jon.howell314@gmail.com>
2022-07-15 14:49:30 +0800

    Added homepage that can query api

--- vue-auth/src/pages/HomePage.vue
+++ vue-auth/src/pages/HomePage.vue
@@ -4,11 +4,21 @@
 
     <div>token</div>
     <div>{{ token }}</div>
+    <div>
+      <button @click="onCallPublicApi">Call Public</button>
+    </div>
+    <div>
+      <button v-if="token" @click="onCallPrivateApi">Call Private</button>
+    </div>
+    <div>
+      <button v-if="token" @click="onCallPrivateScopedApi">Call Private Scoped</button>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+import axios from 'axios';
 import { useAuth0 } from '@auth0/auth0-vue';
 const { getAccessTokenSilently } = useAuth0();
 
@@ -17,4 +27,37 @@ const token = ref(null);
 getAccessTokenSilently()
   .then(resp => (token.value = resp))
   .catch(error => console.error(error));
+
+const onCallPublicApi = () => {
+  axios
+    .get('http://localhost:4000/public')
+    .then(resp => {
+      console.dir(resp);
+    })
+    .catch(e => console.error(e));
+};
+
+const onCallPrivateApi = () => {
+  axios
+    .get('http://localhost:4000/private')
+    .then(resp => {
+      console.dir(resp);
+    })
+    .catch(e => console.error(e));
+};
+
+const onCallPrivateScopedApi = () => {
+  axios
+    .get('http://localhost:4000/private')
+    .then(resp => {
+      console.dir(resp);
+    })
+    .catch(e => console.error(e));
+};
 </script>
+
+<style scoped>
+button {
+  margin: 1rem 0;
+}
+</style>
