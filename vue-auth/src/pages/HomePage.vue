c0a8be352e198d16c13b4b1cb4dd5ced7adc780d
Jonathan Howell <jon.howell314@gmail.com>
2022-07-15 11:04:03 +0800

    added audience to get access token

--- vue-auth/src/pages/HomePage.vue
+++ vue-auth/src/pages/HomePage.vue
@@ -1,3 +1,20 @@
 <template>
-  <div class="home-page">This is the home page</div>
+  <div class="home-page">
+    This is the home page
+
+    <div>token</div>
+    <div>{{ token }}</div>
+  </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+import { useAuth0 } from '@auth0/auth0-vue';
+const { getAccessTokenSilently } = useAuth0();
+
+const token = ref(null);
+
+getAccessTokenSilently()
+  .then(resp => (token.value = resp))
+  .catch(error => console.error(error));
+</script>
