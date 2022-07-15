aff8e7e1a4ab9916edd5076b2320b749d37128b1
Jonathan Howell <jon.howell314@gmail.com>
2022-07-15 11:32:00 +0800

    Added secret file and authentication

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,11 +1,26 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddAuthentication(o =>
+    {
+        o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(o =>
+    {
+        o.Authority = $"https://{builder.Configuration["Auth0:Domain"]}";
+        o.Audience = builder.Configuration["Auth0:Audience"];
+    });
 
 var app = builder.Build();
 
@@ -14,8 +29,10 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
