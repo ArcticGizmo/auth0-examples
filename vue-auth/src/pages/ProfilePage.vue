74ecd9521eeefcff1eed6f7e37e7a8f869fd2a67
Jonathan Howell <jon.howell314@gmail.com>
2022-07-15 15:48:08 +0800

    🧹

--- vue-auth/src/pages/ProfilePage.vue
+++ vue-auth/src/pages/ProfilePage.vue
@@ -1,57 +1,11 @@
 <template>
   <div class="profile-page">
-    This is the profile page
-    <br />
-    <div>Access Token</div>
-    <code>{{ token }}</code>
-    <br />
-    <div>Id Token Claims</div>
-    <pre>{{ idTokenClaims }}</pre>
-    <div>
-      <button @click="onCallPublicApi">Call Public</button>
-    </div>
-    <div>
-      <button v-if="token" @click="onCallPrivateApi">Call Private</button>
-    </div>
-    <div>
-      <button v-if="token" @click="onCallPrivateScopedApi">Call Private Scoped</button>
-    </div>
+    <strong>Welcome to to the profile page. This is everything we know about you</strong>
+    <pre>{{ user }}</pre>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
 import { useAuth0 } from '@auth0/auth0-vue';
-import API from '../code/api';
-const { getAccessTokenSilently, idTokenClaims } = useAuth0();
-
-const token = ref(null);
-
-getAccessTokenSilently()
-  .then(resp => (token.value = resp))
-  .catch(error => console.error(error));
-
-const onCallPublicApi = () => {
-  API.getPublic()
-    .then(resp => console.dir(resp))
-    .catch(e => console.error(e));
-};
-
-const onCallPrivateApi = () => {
-  API.getPrivate(token.value)
-    .then(resp => console.dir(resp))
-    .catch(e => console.error(e));
-};
-
-const onCallPrivateScopedApi = () => {
-  API.getPrivateScoped(token.value)
-    .then(resp => console.dir(resp))
-    .catch(e => console.error(e));
-};
+const { user } = useAuth0();
 </script>
-
-<style scoped>
-button {
-  margin: 1rem 0;
-}
-</style>
--- vue-auth/src/pages/SecurePage.vue
+++ vue-auth/src/pages/SecurePage.vue
@@ -1,5 +1,52 @@
 <template>
   <div class="secure-page">
-    You should only be able to see this page if you are authenticated
+    <strong>This is a secure page</strong>
+    <br />
+    <div>
+      <button @click="onCallPublicApi">Call Public</button>
+    </div>
+    <div>
+      <button v-if="token" @click="onCallPrivateApi">Call Private</button>
+    </div>
+    <div>
+      <button v-if="token" @click="onCallPrivateScopedApi">Call Private Scoped</button>
+    </div>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+import { useAuth0 } from '@auth0/auth0-vue';
+import API from '../code/api';
+const { getAccessTokenSilently } = useAuth0();
+
+const token = ref(null);
+
+getAccessTokenSilently()
+  .then(resp => (token.value = resp))
+  .catch(error => console.error(error));
+
+const onCallPublicApi = () => {
+  API.getPublic()
+    .then(resp => console.dir(resp))
+    .catch(e => console.error(e));
+};
+
+const onCallPrivateApi = () => {
+  API.getPrivate(token.value)
+    .then(resp => console.dir(resp))
+    .catch(e => console.error(e));
+};
+
+const onCallPrivateScopedApi = () => {
+  API.getPrivateScoped(token.value)
+    .then(resp => console.dir(resp))
+    .catch(e => console.error(e));
+};
+</script>
+
+<style scoped>
+button {
+  margin: 1rem 0;
+}
+</style>
