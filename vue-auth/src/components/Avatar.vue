ab057792d414d506be50c02af67c5bdd95375ffa
Jonathan Howell <jon.howell314@gmail.com>
2022-07-15 10:27:16 +0800

    created an avatar handling system

---
+++ vue-auth/src/components/Avatar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="avatar">
+    <img :src="imgData" />
+    <img style="display: none" class="checker" :src="src" @error="onError" @load="onLoaded" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    src: String,
+    fallback: { type: String, default: 'default-avatar.png' },
+  },
+  data() {
+    return {
+      imgData: null,
+    };
+  },
+
+  created() {
+    this.setFallback();
+  },
+
+  methods: {
+    setFallback() {
+      this.imgData = this.fallback;
+    },
+    onError(e) {
+      this.setFallback();
+    },
+    onLoaded() {
+      this.imgData = this.src;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.avatar {
+  width: 2rem;
+  height: 2rem;
+}
+img {
+  width: 100%;
+  height: 100%;
+  border-radius: 100%;
+}
+</style>
